a4f04407ab24c3b5a9acc3024082fb489cdea842
portable <emmanueltemitopedorcas20@gmail.com>
2026-06-26 16:37:01 +0100

    feat: add request-id middleware for log correlation (#805)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,6 @@
 // @ts-nocheck
 
-import { Module } from '@nestjs/common';
+import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
@@ -21,6 +21,7 @@ import { AnalyticsModule } from './analytics/analytics.module';
 import { IntegrationsModule } from './integrations/integrations.module';
 import { AppController } from './app.controller';
 import './common/common.types'; // Load registered enums
+import { RequestIdMiddleware } from './common/request-id.middleware';
 import { AdminModule } from './admin/admin.module';
 import { FraudModule } from './fraud/fraud.module';
 import { SearchModule } from './search/search.module';
@@ -88,4 +89,8 @@ import { AuditModule } from './audit/audit.module';
 
   controllers: [AppController],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdMiddleware).forRoutes('*');
+  }
+}
